6873b891e6083816515a22b3efc949f8ec9406ab
Brandon Thomas <lucky7id@gmail.com>
2016-08-30 16:03:11 -0700

    post component scaffold

--- client/src/ui/components/post.js
+++ client/src/ui/components/post.js
@@ -9,8 +9,26 @@ export default class Post extends React.Component {
 
     render() {
         return (
-            <div>
-                post
+            <div className="post-container">
+              <div className="row">
+                <div className="col-md-11">
+                  <div className="media post-panel">
+                    <div className="media-left">
+                      <img className="media-object" src="https://i.imgflip.com/19pdpk.jpg" alt="..." />
+                    </div>
+                    <div className="media-body">
+                      <h4 className="media-heading">Media heading</h4> some post text
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-1">
+                  <div className="btn-group-vertical votes">
+                    <button className="btn vote up" type="button"><i className="material-icons">arrow_upward</i></button>
+                    <button className="btn vote count" type="button">400</button>
+                    <button className="btn vote down" type="button"><i className="material-icons">arrow_downward</i></button>
+                  </div>
+                </div>
+              </div>
             </div>
         )
     }
--- client/src/ui/main/content.js
+++ client/src/ui/main/content.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import Post from '../components/post';
 
 class MainContent extends React.Component {
 
@@ -11,7 +12,17 @@ class MainContent extends React.Component {
     render() {
         return (
             <div className="main">
-                sidebar
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-12">
+                    <Post />
+                    <Post />
+                    <Post />
+                    <Post />
+                    <Post />
+                  </div>
+                </div>
+              </div>
             </div>
         )
     }
